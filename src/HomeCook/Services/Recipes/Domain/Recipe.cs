20297ca74c903304ec032ac8227cd6d79e3495a4
pavixonpl <pawmin14@gmail.com>
2022-11-26 13:00:37 +0100

    Projects moved

---
+++ src/HomeCook/Services/Recipes/Domain/Recipe.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+
+public record Recipe
+{
+    public Guid Id { get; }
+    public string Title { get; init; }
+    public string Content { get; init; }
+    public string AuthorEmail { get; init; }
+    public string MainPhotoPath { get; init; }
+}
---
+++ src/HomeCook/Services/Recipes/Infrastructure/DbContexts/RecipeDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DbContexts;
+
+public class RecipeDbContext : DbContext
+{
+    DbSet<Recipe> Recipes { get; set; }
+}
