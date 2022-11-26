174a2eba4d9079d281a6b5476ec751225f6ab1ed
pavixonpl <pawmin14@gmail.com>
2022-11-26 16:13:14 +0100

    Working gets for recipe

---
+++ src/HomeCook/Services/Recipes/Web/Serverless.Azure/Functions/GetAllRecipes.cs
@@ -0,0 +1,30 @@
+﻿using Application.Queries.AllRecipes;
+using MediatR;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker;
+using System.Net;
+using System.Text.Json;
+
+namespace Serverless.Azure.Functions;
+
+public class GetAllRecipes
+{
+    private readonly IMediator _mediator;
+
+    public GetAllRecipes(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [Function("GetAllRecipes")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Recipe")] HttpRequestData req)
+    {
+        var res = await _mediator.Send(new AllRecipesQuery());
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        await response.WriteStringAsync(JsonSerializer.Serialize(res));
+
+        return response;
+    }
+}
---
+++ src/HomeCook/Services/Recipes/Web/Serverless.Azure/Functions/GetRecipe.cs
@@ -0,0 +1,32 @@
+﻿using Application.Queries.AllRecipes;
+using MediatR;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker;
+using System.Net;
+using System.Text.Json;
+using Application.Queries.DetailedRecipe;
+
+namespace Serverless.Azure.Functions;
+
+public class GetRecipe
+{
+    private readonly IMediator _mediator;
+
+    public GetRecipe(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [Function("GetRecipe")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Recipe/{id:guid}")] HttpRequestData req, Guid id)
+    {
+        //TODO
+        var res = await _mediator.Send(new DetailedRecipeQuery(id));
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        await response.WriteStringAsync(JsonSerializer.Serialize(res));
+
+        return response;
+    }
+}
