7cab25e999b0dc5ae1c079d1e6f91a6d73625cee
pavixonpl <pawmin14@gmail.com>
2023-12-16 14:50:37 +0100

    ui looks good, got something for the start, now it's time to retrieve data and add it

--- src/HomeCook/Web/UI/Program.cs
+++ src/HomeCook/Web/UI/Program.cs
@@ -1,3 +1,4 @@
+using BlazorStrap;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using UI.Data;
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddBlazorStrap();
 
 var app = builder.Build();
 
