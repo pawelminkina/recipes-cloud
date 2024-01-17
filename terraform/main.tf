eb02c4d8369c5e32328eb08637586ad2ae6b1da8
pavixonpl <pawmin14@gmail.com>
2024-01-17 21:00:44 +0100

    sd

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,18 +18,18 @@ resource "azurerm_storage_account" "storage_account_homecook_files_api" {
 }
 
 # resource "azurerm_service_plan" "service_plan_homecook" {
-#   name                = "example"
-#   resource_group_name = azurerm_resource_group.example.name
-#   location            = azurerm_resource_group.example.location
+#   name                = local.service_plan_homecook_name
+#   resource_group_name = azurerm_resource_group.resource_group_homecook.name
+#   location            = azurerm_resource_group.resource_group_homecook.location
 #   os_type             = "Linux"
 #   sku_name            = "F1"
 # }
 
 # resource "azurerm_linux_web_app" "web_app_homecook_main" {
-#   name                = "example"
-#   resource_group_name = azurerm_resource_group.example.name
-#   location            = azurerm_service_plan.example.location
-#   service_plan_id     = azurerm_service_plan.example.id
+#   name                = local.web_app_homecook_main_name
+#   resource_group_name = azurerm_resource_group.resource_group_homecook.name
+#   location            = azurerm_service_plan.service_plan_homecook.location
+#   service_plan_id     = azurerm_service_plan.service_plan_homecook.id
 
 #   site_config {}
 # }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,10 +4,12 @@ variable "environment" {
 }
 
 locals {
-    homecook_project_name = "homecook"
+    homecook_project_name = "hc"
     short_location = var.location == "North Europe" ? "ne" : "we"
 
     home_cook_rg_name                       = "${var.environment}-${local.short_location}-${local.homecook_project_name}-rg"
     storage_account_homecook_files_api_name = "${var.environment}-${local.short_location}-${local.homecook_project_name}-sa"
 
+    service_plan_homecook_name              = ""
+    web_app_homecook_main_name              = ""
 }
