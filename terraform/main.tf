4d91a154c3ecc2ede35847642fe505afb9cb59f4
pavixonpl <pawmin14@gmail.com>
2024-01-17 20:58:24 +0100

    sd

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,9 +10,26 @@ resource "azurerm_resource_group" "resource_group_homecook" {
 }
 
 resource "azurerm_storage_account" "storage_account_homecook_files_api" {
-  name                     = "storageaccountname"
+  name                     = local.storage_account_homecook_files_api_name
   resource_group_name      = azurerm_resource_group.resource_group_homecook.name
   location                 = azurerm_resource_group.resource_group_homecook.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+# resource "azurerm_service_plan" "service_plan_homecook" {
+#   name                = "example"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+#   os_type             = "Linux"
+#   sku_name            = "F1"
+# }
+
+# resource "azurerm_linux_web_app" "web_app_homecook_main" {
+#   name                = "example"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_service_plan.example.location
+#   service_plan_id     = azurerm_service_plan.example.id
+
+#   site_config {}
+# }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ locals {
     homecook_project_name = "homecook"
     short_location = var.location == "North Europe" ? "ne" : "we"
 
-    home_cook_rg_name = "${var.environment}-${short_location}-${homecook_project_name}-rg"
-    storage_account_homecook_files_api_name = "${var.environment}-${short_location}-${homecook_project_name}-sa"
+    home_cook_rg_name                       = "${var.environment}-${local.short_location}-${local.homecook_project_name}-rg"
+    storage_account_homecook_files_api_name = "${var.environment}-${local.short_location}-${local.homecook_project_name}-sa"
 
 }
