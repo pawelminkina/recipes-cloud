f6597d42fd4e29a4e8d2362ab179c979b8b22dfb
pavixonpl <pawmin14@gmail.com>
2024-01-18 20:56:52 +0100

    dotnet build and test

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,8 +96,8 @@ resource "azurerm_mssql_server" "sql_server_homecook" {
   resource_group_name          = azurerm_resource_group.resource_group_homecook.name
   location                     = azurerm_resource_group.resource_group_homecook.location
   version                      = "12.0"
-  administrator_login          = "4dm1n157r470r"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+  administrator_login          = "4dm1n157r470r" #that set from pipeline
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd" #that set from pipeline
 }
 
 
