b78369113b472b8664a213aa3a99b50388e0e3bd
pavixonpl <pawmin14@gmail.com>
2024-01-23 21:36:00 +0100

    "SCM_DO_BUILD_DURING_DEPLOYMENT" = true

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,6 +95,7 @@ resource "azurerm_linux_function_app" "function_app_recipes" {
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 1
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = true
     "RecipeDatabaseConnectionString" = "Server=tcp:${azurerm_mssql_server.sql_server_homecook.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sql_database_recipes.name};Persist Security Info=False;User ID=${azurerm_mssql_server.sql_server_homecook.administrator_login};Password=${azurerm_mssql_server.sql_server_homecook.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=120;"
   }
 
@@ -114,6 +115,7 @@ resource "azurerm_linux_function_app" "function_app_files" {
   site_config {}
 
   app_settings = {
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = true
     "WEBSITE_RUN_FROM_PACKAGE" = 1
     "FilesDatabaseConnectionString" = "Server=tcp:${azurerm_mssql_server.sql_server_homecook.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sql_database_files.name};Persist Security Info=False;User ID=${azurerm_mssql_server.sql_server_homecook.administrator_login};Password=${azurerm_mssql_server.sql_server_homecook.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=120;"
   }
