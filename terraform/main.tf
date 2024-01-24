53c8f8fa3d42b02775f4bc741a6ceae65627bd3f
pavixonpl <pawmin14@gmail.com>
2024-01-24 19:49:04 +0100

    sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,23 +81,26 @@ resource "azurerm_linux_web_app" "web_app_homecook_main" {
 }
 
 #azure functions
-resource "azurerm_function_app" "function_app_recipes" {
+resource "azurerm_linux_function_app" "function_app_recipes" {
   name                = local.function_app_recipes_name
   resource_group_name = azurerm_resource_group.resource_group_homecook_azure_function.name
   location            = azurerm_resource_group.resource_group_homecook_azure_function.location
+  public_network_access_enabled = true
   enabled = true
   storage_account_name       = azurerm_storage_account.storage_account_homecook_af_storage.name
   storage_account_access_key = azurerm_storage_account.storage_account_homecook_af_storage.primary_access_key
-  app_service_plan_id        = azurerm_service_plan.service_plan_homecook_azure_functions.id
+  service_plan_id            = azurerm_service_plan.service_plan_homecook_azure_functions.id
   
   site_config {
+    application_stack {
+      dotnet_version = "7.0"
+    }
   }
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 1
     "FUNCTIONS_EXTENSION_VERSION"         = "~4"
     "FUNCTIONS_WORKER_RUNTIME"            = "dotnet-isolated"
-    "FUNCTIONS_WORKER_RUNTIME_VERSION"      = "7"
     "linuxFxVersion" = "dotnet-isolated 7"
     "SCM_DO_BUILD_DURING_DEPLOYMENT" = true
     "RecipeDatabaseConnectionString" = "Server=tcp:${azurerm_mssql_server.sql_server_homecook.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sql_database_recipes.name};Persist Security Info=False;User ID=${azurerm_mssql_server.sql_server_homecook.administrator_login};Password=${azurerm_mssql_server.sql_server_homecook.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=120;"
@@ -106,16 +109,21 @@ resource "azurerm_function_app" "function_app_recipes" {
   depends_on = [azurerm_mssql_server.sql_server_homecook]
 }
 
-resource "azurerm_function_app" "function_app_files" {
+resource "azurerm_linux_function_app" "function_app_files" {
   name                = local.function_app_files_name
   resource_group_name = azurerm_resource_group.resource_group_homecook_azure_function.name
   location            = azurerm_resource_group.resource_group_homecook_azure_function.location
+  public_network_access_enabled = true
   enabled = true
   storage_account_name       = azurerm_storage_account.storage_account_homecook_af_storage.name
   storage_account_access_key = azurerm_storage_account.storage_account_homecook_af_storage.primary_access_key
-  app_service_plan_id        =azurerm_service_plan.service_plan_homecook_azure_functions.id
+  service_plan_id            = azurerm_service_plan.service_plan_homecook_azure_functions.id
 
-  site_config {}
+  site_config {
+    application_stack {
+      dotnet_version = "7.0"
+    }
+  }
 
   app_settings = {
     "FUNCTIONS_EXTENSION_VERSION"         = "~4"
